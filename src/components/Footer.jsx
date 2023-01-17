f5c4781cb949808a714c507d6c54233310d076a5
Vincenzo- <viulenzgg@gmail.com>
2023-01-17 15:16:44 +0100

    added logo to the footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,5 +1,6 @@
 // styles
 import "./Footer.scss";
+import RAWG1 from "../assets/img/rawg logo2.png";
 
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import {
@@ -26,6 +27,12 @@ export default function Footer() {
               <FontAwesomeIcon icon={faYoutube} />
             </a>
           </div>
+        </div>
+        <div className='rawg-logo'>
+          <sup>Data provider</sup>
+          <a href='https://rawg.io/' target='_blank'>
+            <img src={RAWG1} alt='' />
+          </a>
         </div>
       </div>
     </footer>
