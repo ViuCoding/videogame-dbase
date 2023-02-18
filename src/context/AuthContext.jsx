d24845d143e82f04fcc0c2725812e9f0f5a205fe
Vincenzo- <viulenzgg@gmail.com>
2023-02-18 10:13:31 +0100

    created AuthContext to share user data across the App

---
+++ src/context/AuthContext.jsx
@@ -0,0 +1,18 @@
+import { useReducer } from "react";
+import { createContext } from "react";
+
+export const AuthContext = createContext();
+
+const authReducer = (state, action) => {};
+
+export default function AuthProvider({ children }) {
+  const [state, dispatch] = useReducer(authReducer, {
+    user: null,
+  });
+
+  return (
+    <AuthContext.Provider value={{ ...state, dispatch }}>
+      {children}
+    </AuthContext.Provider>
+  );
+}
